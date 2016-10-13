2dce5af97773b8427b5dfb13f3bc06e542f826ae
Adriano Zanella Jr <adrianojr11@gmail.com>
2016-10-13 18:46:28 -0300

    Update gram.c

--- Trab_COMP/gram.c
+++ Trab_COMP/gram.c
@@ -4,8 +4,12 @@ extern FILE *yyin;
 
 int main(int argc, char const *argv[])
 {
-	yyin = fopen( argv[ 1], "r")
-	yyparse();
+	yyin = fopen( argv[ 1], "r");
+	if( yyin != NULL){
+		printf("Abrindo arquivo %s. \n", argv[1]);
+		yyparse();
+	}else{
+		printf("Arquivo nao encontrado!");
 	fclose(yyin);
 	return 0;
 }
