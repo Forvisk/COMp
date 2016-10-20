612dd2cffbe7092d5e19fe2c52bf9f2e5c58e7e9
Forvisk <adrianojr11@gmail.com>
2016-10-20 16:19:16 -0200

    ok

--- Trab_COMP/gram.c
+++ Trab_COMP/gram.c
@@ -6,6 +6,13 @@ extern FILE *yyin;
 #define ARQUIVO2 "Exemlo2.c"
 #define ARQUIVO3 "Exemlo3.c"
 
+/*
+*	Adriano Zanella Junior
+*	
+*	gcc gram.c gram.tab.c -o test
+*	gcc gram.c gramV2.tab.c -o test
+*/
+
 int main(int argc, char const *argv[])
 {
 	yyin = fopen( argv[ 1], "r");
